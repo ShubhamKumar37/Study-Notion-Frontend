b24562aadb26b26e4aabe5c7dc91b38f4ed1acd0
it4avijo <sk9818281820@gmail.com>
2025-03-23 12:47:28 +0530

    updated some field for deployment

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,54 +1,58 @@
+const baseUrl = process.env.REACT_APP_BASE_URL;
+console.log("this is the baseUrl = ", baseUrl);
+
 export const userAuthApis = {
-  LOGIN_USER: "/user",
-  SIGNUP_USER: "/user",
-  LOGOUT_USER: "/user/logout",
-  SENDOTP_USER: "/user/sendotp",
-  PASSWORD_CHANGE_USER: "/user/password-change",
-  PASSWORD_TOKEN_USER: "/user/password-token",
-  PASSWORD_RESET_USER: "/user/password-reset",
+  LOGIN_USER: `${baseUrl}/user`,
+  SIGNUP_USER: `${baseUrl}/user`,
+  LOGOUT_USER: `${baseUrl}/user/logout`,
+  SENDOTP_USER: `${baseUrl}/user/sendotp`,
+  PASSWORD_CHANGE_USER: `${baseUrl}/user/password-change`,
+  PASSWORD_TOKEN_USER: `${baseUrl}/user/password-token`,
+  PASSWORD_RESET_USER: `${baseUrl}/user/password-reset`,
 };
 
 export const courseApis = {
-  GET_ALL_COURSE: "/course",
-  GET_COURSE_DETAIL: "/course/:courseId",
-  DELETE_COURSE: "/course/:courseId",
-  UPDATE_COURSE: "/course",
-  CREATE_COURSE: "/course",
+  GET_ALL_COURSE: `${baseUrl}/course`,
+  GET_COURSE_DETAIL: `${baseUrl}/course/:courseId`,
+  DELETE_COURSE: `${baseUrl}/course/:courseId`,
+  UPDATE_COURSE: `${baseUrl}/course`,
+  CREATE_COURSE: `${baseUrl}/course`,
 };
 
 export const sectionApis = {
-  GET_ALL_SECTION: "/course/section/:courseId",
-  CREATE_SECTION: "/course/section/:courseId",
-  UPDATE_SECTION: "/course/section/:sectionId",
-  DELETE_SECTION: "/course/section/:courseId",
+  GET_ALL_SECTION: `${baseUrl}/course/section/:courseId`,
+  CREATE_SECTION: `${baseUrl}/course/section/:courseId`,
+  UPDATE_SECTION: `${baseUrl}/course/section/:sectionId`,
+  DELETE_SECTION: `${baseUrl}/course/section/:courseId`,
 };
 
 export const subSectionApis = {
-  UPDATE_SUBSECTION_TEXT: "/course/subsection",
-  DELETE_SUBSECTION: "/course/subsection/:sectionId",
-  CREATE_SUBSECTION: "/course/subsection/:sectionId",
-  UPDATE_SUBsECTION_VIDEO: "/course/subsection/:subSectionId",
+  UPDATE_SUBSECTION_TEXT: `${baseUrl}/course/subsection`,
+  DELETE_SUBSECTION: `${baseUrl}/course/subsection/:sectionId`,
+  CREATE_SUBSECTION: `${baseUrl}/course/subsection/:sectionId`,
+  UPDATE_SUBsECTION_VIDEO: `${baseUrl}/course/subsection/:subSectionId`,
 };
 
 export const profileApis = {
-  GET_USER_DETAIL: "/profile",
-  UPDATE_PROFILE: "/profile",
-  USER_COURSE_ENROLLED: "/profile/course-enrolled",
-  UPDATE_PROFILE_PICTURE: "/profile/pp",
+  GET_USER_DETAIL: `${baseUrl}/profile`,
+  UPDATE_PROFILE: `${baseUrl}/profile`,
+  USER_COURSE_ENROLLED: `${baseUrl}/profile/course-enrolled`,
+  UPDATE_PROFILE_PICTURE: `${baseUrl}/profile/pp`,
 };
 
 export const categoryApis = {
-  CREATE_CATEGORY: "/category",
-  GET_ALL_CATEGORY: "/category",
-  GET_CATEGORY_DETAIL: "/category/:categoryId",
-  UPDATE_CATEGORY: "/category/:categoryId",
+  CREATE_CATEGORY: `${baseUrl}/category`,
+  GET_ALL_CATEGORY: `${baseUrl}/category`,
+  GET_CATEGORY_DETAIL: `${baseUrl}/category/:categoryId`,
+  UPDATE_CATEGORY: `${baseUrl}/category/:categoryId`,
 };
 
 export const commentApis = {
-  CREATE_COMMENT: "/comment/:courseId",
-  GET_AVERAGE_RATING: "/comment/avg-rating/:courseId",
-  GET_ALL_COURSE_COMMENT: "/comment/:courseId",
-  GET_ALL_COMMENT: "/comment",
-  UPDATE_COMMENT: "/comment/:commentId",
-  DELETE_COMMENT: "/comment/:commentId",
+  CREATE_COMMENT: `${baseUrl}/comment/:courseId`,
+  GET_AVERAGE_RATING: `${baseUrl}/comment/avg-rating/:courseId`,
+  GET_ALL_COURSE_COMMENT: `${baseUrl}/comment/:courseId`,
+  GET_ALL_COMMENT: `${baseUrl}/comment`,
+  UPDATE_COMMENT: `${baseUrl}/comment/:commentId`,
+  DELETE_COMMENT: `${baseUrl}/comment/:commentId`,
 };
+
